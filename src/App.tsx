c0f54be012a3a2ed08d1460715a01431f0578f15
domusss <wengo7003@gmail.com>
2025-07-05 17:59:49 +0000

    сделано

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import {Header} from "./component/Header/Header"
-import { About } from "./component/Main/About/About"
-import { Bridging } from "./component/Main/Bridging/Bridging"
-import {Service} from "./component/Main/Service/Service"
-import {Portfolio} from "./component/Main/Portfolio/Portfolio"
-import {Company} from "./component/Main/Company/Company"
-import {Testimonial} from "./component/Main/Testimonial/Testimonial"
-import { Book } from "./component/Main/Book/Book"
-import { Print } from "./component/Main/Print/Print"
-import { Footer } from "./component/Footer/Footer"
+import { Layout } from "./Layout";
+import { Pithily } from "./Pithily";
 
 
-export function App() {
 
-  return (
-    <div className="container">
-  <Header/>
-  <Bridging/>
-  <About/>
-  <Service/>
-  <Portfolio/>
-  <Company/>
-  <Testimonial/>
-  <Book/>
-  <Print/>
-  <Footer/>
-    </div>
-  )
-}
-
+export const App = () => {
+  return (    
+    <Layout>
+    <Pithily/>
+    </Layout>
+  );
+};
